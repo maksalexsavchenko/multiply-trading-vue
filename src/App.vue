9c19107a13aadeab4c71a686f067a35f00213916
maksalexsavchenko <makskidexom@gmail.com>
2022-10-25 13:28:22 +0300

    add more charts

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,18 @@
       <div class="trading-view-item">
         <TradingViewItem unique-id="trading-view2" :symbol="'BINANCE:BTCUSDT'"/>
       </div>
+      <div class="trading-view-item">
+        <TradingViewItem unique-id="trading-view3" :symbol="'BINANCE:SOLUSDT'"/>
+      </div>
+      <div class="trading-view-item">
+        <TradingViewItem unique-id="trading-view4" :symbol="'BINANCE:XRPBTC'"/>
+      </div>
+      <div class="trading-view-item">
+        <TradingViewItem unique-id="trading-view5" :symbol="'BINANCE:ETHBTC'"/>
+      </div>
+      <div class="trading-view-item">
+        <TradingViewItem unique-id="trading-view6" :symbol="'BINANCE:SOLBTC'"/>
+      </div>
     </div>
   </div>
 </template>
